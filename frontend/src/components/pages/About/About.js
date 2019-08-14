9594d93fb2a91c7b6ece1b49796e392072cda781
vajapravin <vajapravin23@gmail.com>
2019-08-14 08:38:19 +0530

    push to github

---
+++ frontend/src/components/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <Button variant="primary">Primary</Button>
+            <p>This is a description of the site.</p>
+        </div>
+    );
+};
